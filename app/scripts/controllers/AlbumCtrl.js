4d76414b884d410c30f9b6c1c4ad5445502e5978
Simon Lang <simon.gilbert.lang@gmail.com>
2017-03-14 08:36:05 -0700

    Assignment 5 album controller

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,13 @@
+ (function() {
+     function AlbumCtrl() {
+         this.albumData = albumPicasso;
+         
+   }
+
+    angular
+    .module('blocJams')
+    .controller('AlbumCtrl', AlbumCtrl);
+
+
+ })();
+     
